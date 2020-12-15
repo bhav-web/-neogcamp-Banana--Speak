7faee3a73090ceff1a653ac7f2bdfa3ac297c27e
bhav-web <bhav-web@github.com>
2020-12-16 00:10:21 +0530

    functionality of app

--- app.js
+++ app.js
@@ -35,19 +35,4 @@ function errorHandler(error)
 
 
  })
-//  const uri = "https://api.funtranslations.com/translate/minion.json?";
 
-//  function encode(uri)
-//  {
-//      return uri+ "?" + "text= "+ uri
-//  }
-// const encoded = encodeURI(uri);
-// console.log(encoded);
-// // expected output: "https://mozilla.org/?x=%D1%88%D0%B5%D0%BB%D0%BB%D1%8B"
-
-// try {
-//   console.log(decodeURI(encoded));
-//   // expected output: "https://mozilla.org/?x=шеллы"
-// } catch (e) { // catches a malformed URI
-//   console.error(e);
-//
